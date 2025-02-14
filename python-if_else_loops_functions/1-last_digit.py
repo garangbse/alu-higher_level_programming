c92b5801ccb22acf7bd6c2d680f5f8cb1f226197
garangbse <garangbse@gmail.com>
2025-02-14 13:53:50 +0000

    importing random

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,7 @@
 import random
 
 #!/usr/bin/python3
+import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 if number < 0:
@@ -14,4 +15,3 @@ elif last_digit == 0:
     print("and is 0")
 else:
     print("and is less than 6 and not 0")
-    
