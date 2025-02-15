223f8138984b089e7cbdfa71a54ae6853c8b911f
garangbse <garangbse@gmail.com>
2025-02-15 08:52:17 +0000

    correctinvariable added to 8-uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        print("{:c}".format(ord(c) - 32 if ord(c) >= 97 and ord(c) <= 122 
-        else ord(c)), end="")
+        print("{:c}".format(ord(c) - 32 if ord(c) >= 97 and ord(c) <= 122 else ord(c)), end="")
     print("")
+    #!/usr/bin/python3
+    def uppercase(str):
+        for c in str:
+            char = ord(c)
+            if char >= 97 and char <= 122:
+                char -= 32
+            print("{:c}".format(char), end="")
+        print("")
