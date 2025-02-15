4329d4b585baf1156387c79ebf3991a2aa49dc77
garangbse <garangbse@gmail.com>
2025-02-15 11:29:14 +0000

    spacing in 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif argc == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(argc))    
-    for i in range(1, len(argv)):
+        print("{} arguments:".format(argc))
+        for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
