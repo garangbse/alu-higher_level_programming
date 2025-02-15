6a36a81676fa913798029b9e06abfd1885846d74
garangbse <garangbse@gmail.com>
2025-02-15 11:20:55 +0000

    fixing no.1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,12 +1,23 @@
-from calculator_1 import add, sub, mul, div
-
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    a = 10
-    b = 5
+def add(a, b):
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    return (a + b)
+
+
+
+def sub(a, b):
+
+    return (a - b)
+
+
+
+def mul(a, b):
+
+    return (a * b)
+
+
+
+def div(a, b):
+
+    return (a / b)
