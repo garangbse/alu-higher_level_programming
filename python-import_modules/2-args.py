5f38088dfef95cb7c16675020d78c6d25edbe389
garangbse <garangbse@gmail.com>
2025-02-15 11:26:10 +0000

    remove spacing in 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
     elif argc == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(argc))
-    
+        print("{} arguments:".format(argc))    
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
