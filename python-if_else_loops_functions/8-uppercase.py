fc35c15b42b141ca3b9bfcf3a0ca3893a79c8eb8
garangbse <garangbse@gmail.com>
2025-02-15 08:44:28 +0000

    adding bin.python3 to 8-uppersace

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        # Convert character to uppercase if it's lowercase (a-z)
-        # ASCII 'a' = 97, 'z' = 122, difference to uppercase is 32
-        if ord('a') <= ord(c) <= ord('z'):
-            c = chr(ord(c) - 32)
-        print("{:s}".format(c), end="")
+        print("{:c}".format(ord(c) - 32 if ord(c) >= 97 and ord(c) <= 122 else ord(c)), end="")
+    print("")
     
