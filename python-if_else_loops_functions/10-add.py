19fb81d435dc4f1a7044ffcb2935ff9524808005
garangbse <garangbse@gmail.com>
2025-02-15 08:57:51 +0000

    correcting 10

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def add(a, b):
-    """
-    Adds two integers and returns the result
-    Args:
-        a: first integer
-        b: second integer
-    Returns:
-        The sum of a and b
-    """
     return a + b
-    
+   
