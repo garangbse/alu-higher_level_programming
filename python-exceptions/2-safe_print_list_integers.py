1e06bbbda04e52646a74105aba892af377277b91
garangbse <g.buke@alustudent.com>
2025-03-07 16:51:32 +0300

    corecting spacing

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,11 +2,11 @@
 def safe_print_list_integers(my_list=[], x=0):
     """
     Prints the first x elements of a list if they are integers.
-    
+
     Args:
         my_list: The list to print elements from
         x: The number of elements to access
-        
+
     Returns:
         The real number of integers printed
     """
--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -2,10 +2,10 @@
 def raise_exception():
     """
     Function that raises a TypeError exception.
-    
+
     Returns:
         Never returns as it always raises an exception
-    
+
     Raises:
         TypeError: Always raises this exception
     """
