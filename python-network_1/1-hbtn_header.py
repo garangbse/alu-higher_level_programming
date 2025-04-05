2d07b681248fa88e7b181c16a464c9d4c8a60881
garangbse <g.buke@alustudent.com>
2025-04-06 01:50:02 +0300

    reverting

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,16 +1,14 @@
-import sys
-
 #!/usr/bin/python3
 """
-Script that takes in a URL, sends a request and displays the value
-of the X-Request-Id variable found in the header of the response.
+Script to extract X-Request-Id header from response
 """
 import urllib.request
+import sys
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    
+
     with urllib.request.urlopen(url) as response:
         x_request_id = response.getheader('X-Request-Id')
         print(x_request_id)
